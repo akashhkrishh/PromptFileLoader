dfbbdea2a3a0219979fb523bd01d1e3e68383d5c
Akashh Krishh <akashhkrishh@gmail.com>
2025-08-10 01:03:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PromptFileLoader",
-    version="0.1.3",
+    version="1.0.0",
     author="Akashh Krishh",
     author_email="akashhkrishh@gmail.com",
     description="A simple prompt loader supporting YAML and TXT files with caching",
